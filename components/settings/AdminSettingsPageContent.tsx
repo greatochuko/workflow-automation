5e7baa6528685e6754194db9703832a649f29a9f
Great <greatochuko123@gmail.com>
2025-06-08 09:54:40 +0100

    fix: set default tab to client in AdminSettingsPageContent component

--- components/settings/AdminSettingsPageContent.tsx
+++ components/settings/AdminSettingsPageContent.tsx
@@ -12,7 +12,7 @@ export default function AdminSettingsPageContent({
   videoTypes: string[];
   clients: UserType[];
 }) {
-  const [tab, setTab] = useState<"global" | "client">("global");
+  const [tab, setTab] = useState<"global" | "client">("client");
 
   return (
     <div className="mx-auto flex w-[90%] max-w-7xl flex-col gap-6 py-4">
